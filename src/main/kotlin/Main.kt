734f5c74c509ccbd8cf2cdc143318ebde6b5a133
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-05 22:47:01 +0200

    Added multiple tabs and changeListener to Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,20 +5,33 @@ class Main {
     init {
         val frame = JFrame("Flavor")
         val tabbedPane = JTabbedPane()
-        val panel = JPanel()
         frame.isVisible = true
         frame.layout = BorderLayout()
         frame.add(tabbedPane)
-        tabbedPane.addTab("Test", panel)
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         frame.setSize(800, 800)
-        val tabComponent = tabbedPane.getComponentAt(0)
-        if (tabComponent is JPanel) {
-            val button = JButton("Test")
-            button.addActionListener {
-                JOptionPane.showMessageDialog(frame, "Button clicked on Tab 2!")
+
+        // Create the first tab
+        val panel1 = JPanel()
+        val label1 = JLabel("This is the first tab.")
+        panel1.add(label1)
+        tabbedPane.addTab("Tab 1", panel1)
+
+        // Create the second tab
+        val panel2 = JPanel()
+        val label2 = JLabel("This is the second tab.")
+        panel2.add(label2)
+        tabbedPane.addTab("Tab 2", panel2)
+
+        tabbedPane.addChangeListener { event ->
+            val source = event.source
+            if (source is JTabbedPane) {
+                val selectedIndex = source.selectedIndex
+                if (selectedIndex == 1) {
+                    // Do something when the second tab is selected
+                    JOptionPane.showMessageDialog(frame, "Selected Tab 2!")
+                }
             }
-            tabComponent.add(button)
         }
     }
 }
