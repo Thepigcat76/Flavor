49804bdc7a4c5e4d255f3d624db28ac66e7a2a70
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-05 21:43:24 +0200

    Started work on ui in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
+import java.awt.BorderLayout
+import javax.swing.JButton
 import javax.swing.JFrame
 
 fun main() {
-    val frame = JFrame("The main ui");
+    val frame = JFrame("Flavor")
+    val button = JButton("Test")
+    val button2 = JButton("Test2")
+    frame.isVisible = true
+    frame.layout = BorderLayout()
+    frame.add(button, BorderLayout.CENTER)
+    frame.add(button2, BorderLayout.SOUTH)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 }
